cb3b22e3a597b0146cbc1497ec4dcc40ecd67504
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-07-15 14:04:46 +0600

    logo updated

---
+++ src/components/modals/ProductModal.tsx
@@ -0,0 +1,6 @@
+
+export default function ProductModal() {
+  return (
+    <div>ProductModal</div>
+  )
+}
--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <div className={`flex items-center justify-center h-12  text-white font-bold text-2xl  rounded-md`}>
+    <Link to='/' className={`flex items-center justify-center h-12  text-white font-bold text-2xl  rounded-md`}>
       <span>Sportify</span>
       <span className="text-accentColor">Mart</span>
-    </div>
+    </Link>
 
   )
 }
